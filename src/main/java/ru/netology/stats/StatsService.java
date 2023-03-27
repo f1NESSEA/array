1902883d5545987e4e2dfa3b24f69643f8c19db4
Anton Titov <anton.titov97@gmail.com>
2023-03-27 19:48:54 +0300

    Initial Commit

---
+++ src/main/java/ru/netology/stats/StatsService.java
@@ -0,0 +1,73 @@
+package ru.netology.stats;
+
+public class StatsService {
+    public int sumAllSales(int[] sales) {
+        int sumSale = 0;
+        for (int i = 0; i < sales.length; i++) {
+            sumSale = (int) (sumSale + sales[i]);
+        }
+        return sumSale;
+    }
+
+    public int averageSalesPerMonth(int[] sales) {
+        int averageSales = 0;
+        for (int i = 0; i < sales.length; i++) {
+            averageSales = (int) (averageSales + sales[i]);
+        }
+        int averageSalesMonth;
+        averageSalesMonth = averageSales / sales.length;
+        return averageSalesMonth;
+    }
+
+    public int maxSales(int[] sales) {
+        int maxMonth = 0; //
+
+        for (int i = 0; i < sales.length; i++) {
+            if (sales[i] >= sales[maxMonth]) {
+                maxMonth = i;
+            }
+        }
+
+        return maxMonth + 1;
+    }
+
+    public int minSales(int[] sales) {
+        int minMonth = 0;
+
+        for (int i = 0; i < sales.length; i++) {
+            if (sales[i] <= sales[minMonth]) {
+                minMonth = i;
+            }
+        }
+
+        return minMonth + 1;
+    }
+
+    public int minAverageSalesPerMonth(int[] sales) {
+        int min = minAverageSalesPerMonth(sales);
+        int quantityMinMonth = 0;
+
+
+        for (int i = 0; i < sales.length; i++) {
+            if (sales[i] < min) {
+                quantityMinMonth = i;
+            }
+        }
+
+        return quantityMinMonth + 1;
+    }
+
+    public int maxAverageSalesPerMonth(int[] sales) {
+        int max = maxAverageSalesPerMonth(sales);
+        int quantityMaxMonth = 0;
+
+
+        for (int i = 0; i > sales.length; i++) {
+            if (sales[i] > max) {
+                quantityMaxMonth = i;
+            }
+        }
+
+        return quantityMaxMonth + 1;
+    }
+}
---
+++ src/test/java/ru/netology/stats/StatsServiceTest.java
@@ -0,0 +1,56 @@
+package ru.netology.stats;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class StatsServiceTest {
+    @Test
+    public void sumAllSales() {
+        StatsService service = new StatsService();
+
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        int expected = 180;
+        int actual = service.sumAllSales(sales);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void averageSalesPerMonth() {
+        StatsService service = new StatsService();
+
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        int expected = 15;
+        int actual = service.averageSalesPerMonth(sales);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void maxSales() {
+        StatsService service = new StatsService();
+
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        int expected = 6;
+        int actual = service.maxSales(sales);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void minSales() {
+        StatsService service = new StatsService();
+
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        int expected = 9;
+        int actual = service.minSales(sales);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+
+}
